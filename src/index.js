552393d4ff2e6a27f87ad4d7e51fe5b266245e89
Jason Miller <jason@developit.ca>
2017-02-16 22:33:00 -0500

    9. Let's go crazy and add Hot Module Replacement! All we have to do is move our import of `App` and the `render()` call into a function, so that we can re-import changes as they get sent to the browser and re-render the new tree of components. We'll call that function so that we get an initial render like we had before, but then also pass it to Webpack to be called when we get updated components.

--- src/index.js
+++ src/index.js
@@ -7,6 +7,25 @@
 //     h('div', { a: 'b' }, 'foo')
 
 import { h, render } from 'preact';
-import App from './components/app';
 
-render(<App />, document.body);
+
+// this holds our rendered root element so we can re-render in response to HMR updates.
+let root;
+
+
+// Making our app's initialization a function means it's repeatable.
+function init() {
+	// HMR requires that this be a require()
+	let App = require('./components/app').default;
+
+	// render the app and save the new root element:
+	root = render(<App />, document.body, root);
+}
+
+
+// initial render!
+init();
+
+
+// If this is webpack-dev-server, set up HMR :)
+if (module.hot) module.hot.accept('./components/app', init);
